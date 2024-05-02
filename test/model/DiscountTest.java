9885d9733a21d39f9fc4aed0ccfa223d810f7d52
eliasholm8 <67290143+eliasholm8@users.noreply.github.com>
2024-05-02 22:01:32 +0200

    Working on tests

---
+++ test/model/DiscountTest.java
@@ -0,0 +1,21 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscountTest {
+
+    @Test
+    void getItemID() {
+
+    }
+
+    @Test
+    void getSumReduction() {
+    }
+
+    @Test
+    void getPercentageReduction() {
+    }
+}
---
+++ test/model/ItemTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    public void testItemConstructor() {
+        Item item = new Item("123ABC", "Apple", "A tasty apple", 0.75f, 10, 5);
+        assertEquals("123ABC", item.itemID);
+        assertEquals("Apple", item.name);
+        assertEquals("A tasty apple", item.description);
+        assertEquals(0.75f, item.price, 0.001);
+        assertEquals(10, item.vatRate);
+        assertEquals(5, item.quantity);
+    }
+
+    @Test
+    public void test
+}
